90098cd3701926f3c335517e81872bc59dbcd47d
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-05-05 19:12:42 +0900

    stateを追加

--- src/LineLogin.tsx
+++ src/LineLogin.tsx
@@ -6,9 +6,10 @@ interface LineLoginProps {
 }
 
 const LineLogin: React.FC<LineLoginProps> = ({ clientId, redirectUri }) => {
+  const state = Math.random().toString(36).substring(7);
   const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri
-  )}&scope=profile+openid`;
+  )}&scope=profile+openid&state=${state}`;
   return <a href={loginUrl}>LINEでログイン</a>;
 };
 
