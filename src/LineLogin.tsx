ab4862fc5f954ccfeb1b4bca9fc6fd64b247b638
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-05-05 19:04:26 +0900

    refirectUriを動的に

--- src/LineLogin.tsx
+++ src/LineLogin.tsx
@@ -6,11 +6,9 @@ interface LineLoginProps {
 }
 
 const LineLogin: React.FC<LineLoginProps> = ({ clientId, redirectUri }) => {
-  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=https%3A%2F%2Fgoogle.com&scope=profile+openid`;
-  //   const loginUrl =
-  //     'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz';
-  console.log(loginUrl);
-  //https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1661050349&redirect_uri=https%253A%252F%252Fgoogle.com&scope=profile+openid
+  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=profile+openid`;
   return <a href={loginUrl}>LINEでログイン</a>;
 };
 
