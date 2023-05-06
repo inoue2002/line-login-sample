2b0d7771f51d2df4a7ad46c484c18c2f5dcce656
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-05-06 09:58:04 +0900

    エンコードURLを確認する

--- src/LineLogin.tsx
+++ src/LineLogin.tsx
@@ -7,7 +7,10 @@ interface LineLoginProps {
 
 const LineLogin: React.FC<LineLoginProps> = ({ clientId, redirectUri }) => {
   const state = Math.random().toString(36).substring(7);
-  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=profile+openid&state=${state}`;
+  console.log(redirectUri, encodeURIComponent(redirectUri));
+  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=profile+openid&state=${state}`;
   return <a href={loginUrl}>LINEでログイン</a>;
 };
 
