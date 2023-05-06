334f7dc5763bec06e200079d40033b25846343d5
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-05-06 11:58:21 +0900

    add log

--- src/Callback.tsx
+++ src/Callback.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 import React, { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 interface CallbackProps {
   clientId: string;
@@ -10,7 +10,7 @@ interface CallbackProps {
 }
 
 const Callback: React.FC<CallbackProps> = ({ clientId, clientSecret, redirectUri }) => {
-  const history = useHistory();
+  //   const history = useHistory();
   const location = useLocation();
   const [user, setUser] = React.useState<{ userId: string; displayName: string; pictureUrl: string } | null>(null);
   const [accessToken, setAccessToken] = React.useState<string>('');
@@ -36,12 +36,14 @@ const Callback: React.FC<CallbackProps> = ({ clientId, clientSecret, redirectUri
       const response = await axios.post('https://api.line.me/oauth2/v2.1/token', qs.stringify(data), config);
 
       console.log(response.data);
+      console.log(response.data.access_token, 'response.data.access_token');
       setAccessToken(response.data.access_token);
       setIdToken(response.data.id_token);
       //   history.push('/');
     };
 
     const getProfile = async () => {
+      console.log(accessToken, 'accessToken');
       const response = await axios.get('https://api.line.me/v2/profile', {
         headers: { authorization: `Bearer ${accessToken}` },
       });
