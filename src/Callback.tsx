2fc4ecaa4b29df7f0a8baad5a08e3e59540a3aff
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-05-06 10:26:27 +0900

    友達追加を取得する

--- src/Callback.tsx
+++ src/Callback.tsx
@@ -11,13 +11,16 @@ interface CallbackProps {
 const Callback: React.FC<CallbackProps> = ({ clientId, clientSecret, redirectUri }) => {
   const history = useHistory();
   const location = useLocation();
-
+  const [user, setUser] = React.useState<{ userId: string; displayName: string; pictureUrl: string } | null>(null);
+  const [friendshipStatusChanged, setFriendshipStatusChanged] = React.useState<boolean>(false);
   useEffect(() => {
     const code = new URLSearchParams(location.search).get('code');
     if (!code) {
       return;
     }
 
+    const friendship_status_changed = new URLSearchParams(location.search).get('friendship_status_changed');
+    setFriendshipStatusChanged(friendship_status_changed === 'true');
     const getToken = async () => {
       const response = await axios.post('https://api.line.me/oauth2/v2.1/token', null, {
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
@@ -31,13 +34,46 @@ const Callback: React.FC<CallbackProps> = ({ clientId, clientSecret, redirectUri
       });
 
       console.log(response.data);
-      history.push('/');
+      //   history.push('/');
     };
 
     getToken();
+
+    const getProfile = async () => {
+      const response = await axios.get('https://api.line.me/v2/profile', {
+        headers: { authorization: `Bearer ${code}` },
+      });
+
+      console.log(response.data);
+
+      const { userId, displayName, pictureUrl } = response.data as {
+        userId: string;
+        displayName: string;
+        pictureUrl: string;
+      };
+      console.log(userId, displayName, pictureUrl);
+      return {
+        userId,
+        displayName,
+        pictureUrl,
+      };
+    };
+
+    getProfile();
   }, [history, location.search, clientId, clientSecret, redirectUri]);
 
-  return <div>ログイン処理中...</div>;
+  // ログインが完了するまでは「ログイン中」を表示、ログイン後はユーザー情報を表示
+  return (
+    <div>
+      {user ? (
+        <div>
+          {user.displayName} <div>友達追加状況：{friendshipStatusChanged}</div>
+        </div>
+      ) : (
+        <div>ログイン中</div>
+      )}
+    </div>
+  );
 };
 
 export default Callback;
--- src/LineLogin.tsx
+++ src/LineLogin.tsx
@@ -8,7 +8,7 @@ interface LineLoginProps {
 const LineLogin: React.FC<LineLoginProps> = ({ clientId, redirectUri }) => {
   const state = Math.random().toString(36).substring(7);
   console.log(redirectUri, encodeURIComponent(redirectUri));
-  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=profile+openid&state=${state}`;
+  const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=profile+openid&state=${state}&bot_prompt=aggressive&initial_amr_display=lineqr&disable_auto_login=true`;
   return <a href={loginUrl}>LINEでログイン</a>;
 };
 
