33c55d45df3114332fef46234f20ee20e7cd6a61
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-08-05 12:23:09 +0900

    APIにリクエスト送信

--- src/LineLogin.tsx
+++ src/LineLogin.tsx
@@ -1,8 +1,8 @@
+import { Profile } from '@liff/get-profile';
 import liff from '@line/liff';
-import { Profile } from "@liff/get-profile";
 import { LiffMockPlugin } from '@line/liff-mock';
-import React, { useEffect, useState } from 'react';
-
+import axios from 'axios';
+import React, { useEffect } from 'react';
 // interface LineLoginProps {
 //   clientId: string;
 //   redirectUri: string;
@@ -16,28 +16,39 @@ import React, { useEffect, useState } from 'react';
 
 const LineLogin: React.FC = () => {
   const [userProfile, setUserProgfile] = React.useState<Profile | null>(null);
+  const [res, setRes] = React.useState("");
   liff.use(new LiffMockPlugin());
 
   // liffId: import.meta.env.VITE_LIFF_ID
 
   useEffect(() => {
-    console.log('ログインします');
-    // if (!liff.isInClient()) liff.login();
+    const isMock = process.env.NODE_ENV === 'development' ? true : false;
     liff
       .init({
         liffId: '1661050349-93ygRPP8',
         // @ts-ignore
-        mock: false // enable mock mode
+        mock: isMock,
       })
       .then(async () => {
         if (!liff.isLoggedIn()) {
           liff.login();
         }
-        console.log('成功');
+        const idToken = liff.getIDToken();
         // setMessage('LIFF init succeeded.');
         const profile = await liff.getProfile();
         console.log({ profile });
         setUserProgfile(profile);
+
+        // window.location.srarchを送る
+
+        // DBに保存する
+
+        // にaxiosでリクエストを送る
+        const apiRes = await axios.post('https://ai-chatkun-dev.azurewebsites.net/api/line-inflow', {
+          idToken,
+          params: window.location.search,
+        });
+        setRes(apiRes.statusText);
       })
       .catch((e: Error) => {
         console.log(e);
@@ -51,7 +62,12 @@ const LineLogin: React.FC = () => {
   // const loginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
   //   redirectUri
   // )}&scope=profile+openid&state=${state}&bot_prompt=aggressive&initial_amr_display=lineqr&disable_auto_login=true`;
-  return <div>ログイン {userProfile ? userProfile.displayName : null}</div>;
+  return (
+    <div>
+      ログイン {userProfile ? userProfile.displayName : null}
+      <div>{res ? res : 'なし'}</div>
+    </div>
+  );
 };
 
 export default LineLogin;
